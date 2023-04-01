d9a74354e25d3ff82feef5dd7f1ac4390f2409e6
Matt Rostad <matt@rostad.ca>
2023-04-01 16:12:47 -0700

    Redirect Short URLs

--- express_server.js
+++ express_server.js
@@ -6,22 +6,21 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 function generateRandomString() {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const charactersLength = characters.length;
-  for ( let i = 0; i < 6; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
 
 console.log(generateRandomString()); // Example output: "G4t9Fj"
 
-
-
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/urls", (req, res) => {
@@ -36,21 +35,33 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const key = generateRandomString()
+  urlDatabase[key] = req.body.longURL
+  console.log(urlDatabase)
+  res.redirect(`/urls/${key}`); 
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  res.redirect(longURL);
 });
 
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
- app.get("/urls/:id", (req, res) => {
-  const id = req.params.id
-  const templateVars = { id: req.params.id, longURL: urlDatabase[id]/* What goes here? */ };
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[id] /* What goes here? */,
+  };
   res.render("urls_show", templateVars);
 });
- app.get("/fetch", (req, res) => {
+app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
